c284192e8a2778350b9e2a8419e560f112981922
Matt Greff <greff.matt@icloud.com>
2018-05-17 19:19:28 +0000

    missed a file from last commit

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-const PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const express = require("express");
 const ejs = require("ejs");
+const PORT = process.env.PORT || 8080; // default port 8080
 const app = express();
 
-function generateRandomString() {
-  return Math.random().toString(36).substring(2, 8);
-}
-
 app.set('view engine', 'ejs');
-
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -22,63 +13,127 @@ app.use(bodyParser.urlencoded({
 
 app.use(cookieParser());
 
+app.use('/', (req, res, next) => {
+  res.locals.user_id = req.cookies.id;
+  console.log('local:', res.locals.user_id)
+  next();
+});
+
+function generateRandomString() {
+  return Math.random()
+    .toString(36)
+    .substring(2, 8);
+}
+//////////////////////////////////////////////debugging this///////////////////////////////////////////////////
+function emailAuthenticator(form, cookie) {
+  if (form.email === '' || form.password === '') {
+    return 'You left it blank yo!';
+  }
+  for (let id in users) {
+    console.log(form.password);
+    if (users[id].email === form.email) {
+      return (cookie && users[id].password === form.password) ? users[id] : false;
+    } // return value is switched for cookie authenticattion
+  }
+  return cookie ? false : true;
+}
+//////////////////////////////////////////////debugging this///////////////////////////////////////////////////
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const users = {
+  "user1": {
+    id: "user1",
+    email: "user@example.com",
+    password: "asdf"
+  },
+  "d3s2a1": {
+    id: "d3s2a1",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 app.get("/urls", (req, res) => {
-  console.log(req.cookies.username)
-  let templateVars = {
+  const templateVars = {
     urls: urlDatabase,
-    username: req.cookies.username
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  console.log(req.cookies.username);
-  let templateVars = {
-    username: req.cookies.username
-  };
-  res.render("urls_new", templateVars);
+  res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
-  var newId = generateRandomString();
-  let longURL = req.body.longURL
+  const newId = generateRandomString();
+  const longURL = req.body.longURL
   urlDatabase[newId] = req.body.longURL
   res.redirect('urls');
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {
-    shortURL: req.params.id,
-    username: req.cookies.username
+  const templateVars = {
+    shortURL: req.params.id
   };
   res.render("urls_show", templateVars);
 });
 
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect(301, "/urls")
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect(301, "/urls")
-})
+app.post("/register", (req, res, next) => {
+  if (emailAuthenticator(req.body, false)) {
+    const id = generateRandomString();
+    users[id] = {
+      id: id,
+      email: req.body.email,
+      password: req.body.password
+    }
+  } else {
+    res.status(404);
+  }
+  console.log(users);
+  res.redirect(301, "/urls");
+});
 
-app.post("/urls/:id/edit", (req, res) =>{
+app.get("/login", (req, res) => {
+  res.render("login")
+});
+//////////////////////////////////////////////debugging this///////////////////////////////////////////////////
+app.post("/login", (req, res) => {
+  console.log('debugging of appPostLogin', emailAuthenticator(req.body, true));
+  if (emailAuthenticator(req.body, true)) {
+    res.cookie('id', emailAuthenticator(req.body, true));
+    res.redirect("/urls");
+  } else {
+    res.render("login");
+  }
+});
+//////////////////////////////////////////////debugging this///////////////////////////////////////////////////
+app.post("/logout", (req, res) => {
+  res.clearCookie("id");
+  res.redirect(301, "/login")
+});
+
+app.post("/urls/:id/edit", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls");
 });
 
-app.post("/urls/:id/delete", (req, res) =>{
+app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
-  res.redirect("urls")
+  res.redirect("/urls")
 });
 
 app.get(`/u/:shortURL`, (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny App listening on port ${PORT}!`);
 });
